d9bfbbd400855db9d7ce722e2566a9fc635119f2
alkorithmaker <zzty12345@naver.com>
2025-12-03 16:28:52 +0900

    Q7) 단일 반복문을 작성해 봅시다.
    구구단과 대소문자출력

---
+++ c/test/quiz07.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    for(int i=1;i<=9;i++)
+        printf("3 * %d = %d\n",i,i*3);  //구구단 3단
+
+    for(int i=0;i<26;i++)
+        printf("%c ",i+65);  //알파벳 대문자 출력
+
+    for(int i=0;i<26;i++)
+        printf("%c ",i+97);  //알파벳 소문자 출력
+
+}
+
