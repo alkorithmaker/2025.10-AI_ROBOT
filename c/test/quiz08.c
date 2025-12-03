655ffdc96f84fb8a7b69f96605d06c41b8280936
alkorithmaker <zzty12345@naver.com>
2025-12-03 16:31:12 +0900

    Q8) 2단 ~ 9단까지 Full 출력하라~~!!!
    이중 반복문 O(N^2)

---
+++ c/test/quiz08.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main(){
+
+	for (int i = 2; i < 10; i++){
+      for (int j = 1; j < 10; j++) {
+
+			  printf("%d * %d = %d\n", i, j, i * j); // 구구단 2단부터 Full로 출력하라.
+	    }
+
+		printf("--------------------\n");
+
+	}
+
+
+
+
+
+	return 0;
+
+}
